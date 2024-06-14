7c33581d82f96e484ce4a4b049f02012ee9c776b
Hussnain Cheema <76845698+hussnaincheema@users.noreply.github.com>
2024-06-14 09:57:28 +0600

    Java Important Patterns Commited

---
+++ CodingPatterns.java
@@ -0,0 +1,195 @@
+
+// /////////Print the Pattern of Solid Rectangle////////////
+//                      * * * * *
+//                      * * * * * 
+//                      * * * * * 
+//                      * * * * * 
+
+public class CodingPatterns{
+    public static void main(String args[]){
+        int n = 4; //rows
+        int m = 5; //columns
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// /////////Print the Pattern of Hollow Rectangle////////////
+//                      * * * * *
+//                      *       * 
+//                      *       * 
+//                      * * * * * 
+
+public class CodingPatterns{
+    public static void main(String args[]){
+        int n=4;
+        int m=5;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(i==1 || j==1 || i==n || j==m){
+                    System.out.print("*" + " ");
+                } else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// /////////Print the Pattern of Half Pyramid////////////
+//                      * 
+//                      * *  
+//                      * * * 
+//                      * * * * 
+//                      * * * * *
+
+public class CodingPatterns{
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
+// /////////Print the Pattern of Inverted Half Pyramid////////////
+//                      * * * *
+//                      * * *  
+//                      * *  
+//                      * 
+
+public class CodingPatterns{
+    public static void main(String args[]){
+        int n=4;
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// /////////Print the Pattern of Inverted Half Pyramid (Rotated by 180deg) ////////////
+//                           *
+//                         * * 
+//                       * * * 
+//                     * * * * 
+
+
+public class CodingPatterns{
+    public static void main(String args[]){
+        int n=4;
+        for(int i=1; i<=n; i++){
+            // inner loop for spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+                // inner loop for star printing
+                for(int k=1; k<=i; k++){
+                    System.out.print("*");
+                }
+            
+            System.out.println();
+        }
+    }
+}
+
+//    ////////////Print the Number Pattern ///////////////
+                    // 1
+                    // 1 2
+                    // 1 2 3
+                    // 1 2 3 4 
+                    // 1 2 3 4 5
+
+public class CodingPatterns {
+
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//    ////////////Print the Number Pattern ///////////////
+                    // 1 2 3 4 5
+                    // 1 2 3 4
+                    // 1 2 3
+                    // 1 2
+                    // 1
+
+public class CodingPatterns {
+
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//    ////////////Print the Number Pattern (Floyd's Triangle) ///////////////
+
+                // 1
+                // 2  3
+                // 4  5  6
+                // 7  8  9  10
+                // 11 12 13 14 15
+
+public class CodingPatterns {
+
+    public static void main(String args[]){
+        int n=5;
+        int number = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(number + "  ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+//    ////////////Print the Number Pattern ///////////////
+                    // 1
+                    // 0 1
+                    // 1 0 1
+                    // 0 1 0 1
+                    // 1 0 1 0 1
+
+public class CodingPatterns {
+
+    public static void main(String args[]){
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                int sum = i+j;
+                if(sum % 2 ==0){
+                    System.out.print("1" + " ");
+                }else{
+                    System.out.print("0" + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
