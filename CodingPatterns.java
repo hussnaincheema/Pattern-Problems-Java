4ccc8a48a6ba2b070f8aab480187713044dabdb4
Hussnain Cheema <76845698+hussnaincheema@users.noreply.github.com>
2024-06-15 10:46:00 +0600

    CodingPatterns Final Commited

--- CodingPatterns.java
+++ CodingPatterns.java
@@ -508,3 +508,34 @@ public class CodingPatterns {
         }
     }
 }
+
+
+// // // // //    ////////////Print the Given Pattern ///////////////
+
+
+                            1
+                            2 4
+                            3 6 9
+                            2 4
+                            1
+
+
+public class CodingPatterns{
+    public static void main(String args[]){
+        int n=3;
+        //Upper half
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i*j + " ");
+            }
+            System.out.println();
+        }
+        //lower half
+        for(int i=2; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(i*j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
